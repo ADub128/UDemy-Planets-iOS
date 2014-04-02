03f42326bdaa7d9ad6f196852d030b65f0e0e479
Art Wong <ADub128@AW-MBP2012-5.local>
2014-04-01 17:22:56 -0700

    Initial Commit

---
+++ Out of This World/OTWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OTWAppDelegate.h
+//  Out of This World
+//
+//  Created by Art Wong on 4/1/14.
+//  Copyright (c) 2014 Art Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OTWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Out of This World/OTWViewController.h
@@ -0,0 +1,13 @@
+//
+//  OTWViewController.h
+//  Out of This World
+//
+//  Created by Art Wong on 4/1/14.
+//  Copyright (c) 2014 Art Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OTWViewController : UIViewController
+
+@end
