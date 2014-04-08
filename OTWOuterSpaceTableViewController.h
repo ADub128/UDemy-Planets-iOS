b60e173659b621d6eb6bfedb8d2a82f973a58e41
Art Wong <ADub128@AW-MBP2012-7.local>
2014-04-08 14:28:31 -0700

    first commit

---
+++ OTWOuterSpaceTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  OTWOuterSpaceTableViewController.h
+//  Out of This World
+//
+//  Created by Art Wong on 4/1/14.
+//  Copyright (c) 2014 Art Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OTWOuterSpaceTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *planets;
+
+@end
