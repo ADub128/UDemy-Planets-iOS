8b3a27f574146036460178e774d85f0430bd666b
Art Wong <ADub128@AW-MBP2012-7.local>
2014-04-08 14:48:00 -0700

    added a test class

---
+++ Out of This World/OTWTest.h
@@ -0,0 +1,13 @@
+//
+//  OTWTest.h
+//  Out of This World
+//
+//  Created by Art Wong on 4/8/14.
+//  Copyright (c) 2014 Art Wong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OTWTest : NSObject
+
+@end
---
+++ Out of This World/OTWTest.m
@@ -0,0 +1,13 @@
+//
+//  OTWTest.m
+//  Out of This World
+//
+//  Created by Art Wong on 4/8/14.
+//  Copyright (c) 2014 Art Wong. All rights reserved.
+//
+
+#import "OTWTest.h"
+
+@implementation OTWTest
+
+@end
